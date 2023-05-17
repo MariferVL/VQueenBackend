780c0910289d8458d87302797b7d71faac3709c8
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-17 01:07:19 -0400

    update boom from hapi library

--- src/routes/getMenu.js
+++ src/routes/getMenu.js
@@ -1,3 +1,4 @@
+import Boom from "@hapi/boom";
 import { fakeMenu } from "./fake-data";
 
 export const getMenuRoute = {
@@ -5,7 +6,10 @@ export const getMenuRoute = {
     path: '/api/menu/{id}',
     handler: (req, h) => {
         const id = req.params.id;
-        return fakeMenu.find(menu => menu.id === id);
+        const menu =  fakeMenu.find(menu => menu.id === id);
+        if (!menu) throw Boom.notFound(`Menu does not exist with id ${id}`);
+        return menu;
+
     }
 
 }
