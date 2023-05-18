90e1d264e235bd65df9e09d8321a6e788239caa5
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-17 22:52:49 -0400

    create update menu route

---
+++ src/routes/updateMenu.js
@@ -0,0 +1,26 @@
+import { db } from "../database";
+
+export const updateMenuRoute = {
+    method: 'POST',
+    path: '/api/menu/{id}',
+    handler: async (req, h) => {
+        const { id } = req.params;
+        const { name, image, description, ingredients, allergens, price } = req.payload;
+        const adminId = 'VQ237865';
+        await db.query(
+            `
+            UPDATE menu 
+                SET name=?, image=?, description=?, ingredients=?, allergens=?, price=?
+                WHERE idmenu=? AND admin_id=?
+            `, [name, image, description, ingredients, allergens, price, id, adminId]);
+
+        const { results } = await db.query(`
+                SELECT * FROM menu WHERE idmenu=? AND admin_id=?`,
+            [id, adminId],
+
+        );
+
+        return results[0];
+
+    }
+}
