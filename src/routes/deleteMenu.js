5555aa4446e3e5845f4a71505c7345b1e9a82032
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-21 15:28:00 -0400

    add try/cath to delete menu

--- src/routes/deleteMenu.js
+++ src/routes/deleteMenu.js
@@ -1,19 +1,18 @@
 import { db } from "../database";
 
-
 export const deleteMenuRoute = {
-    method: 'DELETE',
-    path: '/api/menu/{id}',
-    handler: async (req, h) => {
+  method: 'DELETE',
+  path: '/api/menu/{id}',
+  handler: async (req, h) => {
+    const { id } = req.params;
+    const adminId = 'VQ237865';
 
-        const { id } =  req.params;
-        const { name, image, description, ingredients, allergens, price } = req.payload;
-        const adminId = 'VQ237865';
-       await db.query(
-            `DELETE FROM menu WHERE idmenu=?`,
-            [id],
-        );
-
-        return { message: 'Product deleted successfully!'}
+    try {
+      await db.query('DELETE FROM menu WHERE idmenu = ?', [id]);
+      return { message: `Product deleted successfully!`};
+    } catch (error) {
+      console.error('Error deleting menu item:', error);
+      return h.response('Internal Server Error').code(500);
     }
-}
+  }
+};
