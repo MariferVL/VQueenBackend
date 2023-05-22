6a91ffc4aa11ae1b2699a5566a56b1c5e114ecac
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-21 20:41:47 -0400

    create menu working 100%

--- src/routes/createNewMenu.js
+++ src/routes/createNewMenu.js
@@ -6,8 +6,9 @@ export const createNewMenuRoute = {
     method: 'POST',
     path: '/api/menu',
     handler: async (req, h) => {
-        const id = 'VQ23' + uuid();
-        const { name = '', image = '', description = '', ingredients = '', allergens = '', price = 0 } = req.payload;
+        const idmenu = 'VQ23' + uuid();
+        console.log('req.payload in create: ' , req.payload);
+        const { name = '', imgRelPath = '', description = '', ingredients = '', allergens = '', price = 0 } = req.payload;
         const userId = '12345';
         const sales = 0;
 
@@ -15,10 +16,9 @@ export const createNewMenuRoute = {
             INSERT INTO menu (idmenu, name, imgRelPath, description, ingredients, allergens, price, sales)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?); 
             `,
-            [id, name, image, description, ingredients, allergens, price, sales]
+            [idmenu, name, imgRelPath, description, ingredients, allergens, price, sales]
         );
-
-        return {id, name, image, description, ingredients, allergens, price, sales};
+        return {idmenu, name, imgRelPath, description, ingredients, allergens, price, sales};
 
     }
 }
